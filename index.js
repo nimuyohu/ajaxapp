5ae4edc25228c4977eb9468d330e01d36dd1f594
nimurarikuto <nimuyohu0105@gmail.com>
2020-11-28 13:06:56 +0900

    HTML文字列をエスケープさせる

--- index.js
+++ index.js
@@ -9,25 +9,49 @@ fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         if (!response.ok){
             console.error("エラーレスポンス",response);
         } else {
+            // json形式にパースして渡す
             return response.json().then(userInfo => {
                 console.log(userInfo)
-                const view = `
-<h4>${userInfo.name} (@${userInfo.login})</h4>
-<img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-<dl>
-    <dt>Location</dt>
-    <dd>${userInfo.location}</dd>
-    <dt>Repositories</dt>
-    <dd>${userInfo.public_repos}</dd>
-</dl>
-`;
+                const view = escapeHTML`
+                            <h4>${userInfo.name} (@${userInfo.login})</h4>
+                            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                            <dl>
+                                <dt>Location</dt>
+                                <dd>${userInfo.location}</dd>
+                                <dt>Repositories</dt>
+                                <dd>${userInfo.public_repos}</dd>
+                            </dl>
+                            `;
 
-const result = document.getElementById('result')
-result.innerHTML = view
+            const result = document.getElementById('result')
+            result.innerHTML = view
             })
         }
         }).catch(error => {
             console.error(error); 
         });
+        
 }
+
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i - 1];
+        if (typeof value === "string") {
+            return result + htmlspecialchars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    })
+}
+// <>や、&などの特殊な文字列は意図しない構造のHTMLになるかもしれないたため、UTF8に変換する
+// ライブラリを使うのが一般だけど、下のように実装。上で使ったのはhtmlspecialcharsというライブラリ
+// https://github.com/teppeis/htmlspecialchars#for-browsers
+// function escapeSpecialChars(str) {
+//     return str
+//         .replace(/&/g, "&amp;")
+//         .replace(/</g, "&lt;")
+//         .replace(/>/g, "&gt;")
+//         .replace(/"/g, "&quot;")
+//         .replace(/'/g, "&#039;");
+// }
 
